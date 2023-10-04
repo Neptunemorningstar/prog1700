50d885d0a78b560f7ad76328ef6fe2e8a1ae2079
Neptune Morningstar <coreycrim@icloud.com>
2023-10-04 10:17:05 -0300

    as do 10/04/2023

---
+++ roman.ps1
@@ -0,0 +1,11 @@
+[int]$number = Read-host = "enter a number between 1 - 10"
+if ($number -eq 1 ){ Write-host "I"}
+if ($number -eq 2) { write-host "II"}
+if ($Number -eq 3) { Write-Host "III"}
+if ($Number -eq 4) { Write-host "IV"}
+if ($Number -eq 5) {Write-host "V"} 
+if ($Number -eq 6) {Write-host "VI"} 
+if ($Number -eq 7) {Write-host "VII"} 
+if ($Number -eq 8) {Write-host "VIII"} 
+if ($Number -eq 9) {Write-host "IX"} 
+if ($Number -eq 10) {Write-host "X"} 
